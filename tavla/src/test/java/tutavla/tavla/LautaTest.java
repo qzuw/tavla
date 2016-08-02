52c6367821c52bbe7f18e7e235405b7048954049
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-08-02 18:23:50 +0300

    testit pelilaudalle

--- tavla/src/test/java/tutavla/tavla/LautaTest.java
+++ tavla/src/test/java/tutavla/tavla/LautaTest.java
@@ -17,22 +17,22 @@ import static org.junit.Assert.*;
  * @author ttuotila
  */
 public class LautaTest {
-    
+
     public LautaTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -42,4 +42,42 @@ public class LautaTest {
     //
     // @Test
     // public void hello() {}
+    @Test
+    public void asetaNappula() {
+        Lauta lauta = new Lauta();
+        Pelaaja pelaaja = new Pelaaja();
+        Nappula nappula = new Nappula(pelaaja);
+
+        assertEquals(lauta.nappuloitaRuudussa(12), 0);
+        lauta.asetaNappula(nappula, 12);
+        assertEquals(lauta.nappuloitaRuudussa(12), 1);
+    }
+
+    @Test
+    public void ruutuPelaajan() {
+        Lauta lauta = new Lauta();
+        Pelaaja pelaaja = new Pelaaja();
+        Pelaaja toinen = new Pelaaja();
+        Nappula nappula = new Nappula(pelaaja);
+
+        assertFalse(lauta.ruutuPelaajalla(12, pelaaja));
+        lauta.asetaNappula(nappula, 12);
+        assertTrue(lauta.ruutuPelaajalla(12, pelaaja));
+        assertFalse(lauta.ruutuPelaajalla(12, toinen));
+        lauta.siirraNappula(12, 0);
+        assertFalse(lauta.ruutuPelaajalla(12, pelaaja));
+        assertFalse(lauta.ruutuPelaajalla(0, pelaaja));
+        lauta.siirraNappula(0, 1);
+        assertTrue(lauta.ruutuPelaajalla(1, pelaaja));
+        assertFalse(lauta.ruutuPelaajalla(1, toinen));
+        lauta.siirraNappula(1, 24);
+        assertFalse(lauta.ruutuPelaajalla(1, pelaaja));
+        assertTrue(lauta.ruutuPelaajalla(24, pelaaja));
+        assertFalse(lauta.ruutuPelaajalla(24, toinen));
+        lauta.siirraNappula(24, 25);
+        assertFalse(lauta.ruutuPelaajalla(24, pelaaja));
+        assertFalse(lauta.ruutuPelaajalla(25, pelaaja));
+        assertFalse(lauta.ruutuPelaajalla(25, toinen));
+        
+    }
 }
