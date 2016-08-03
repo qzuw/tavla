d7303801e35a976c0a4b6b874131cae76b7284f4
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-08-03 18:54:26 +0300

    logiikan alkua, asetetaan nappulat laudalle

---
+++ tavla/src/main/java/tutavla/tavla/logiikka/Logiikka.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutavla.tavla.logiikka;
+
+import tutavla.tavla.domain.Lauta;
+import tutavla.tavla.domain.Nappula;
+import tutavla.tavla.domain.Noppa;
+import tutavla.tavla.domain.Pelaaja;
+
+/**
+ *
+ * @author ttuotila@cs
+ */
+public class Logiikka {
+
+    private Lauta lauta;
+    private Pelaaja pelaaja;
+    private Pelaaja tietokone;
+    private Noppa noppa1;
+    private Noppa noppa2;
+    private boolean pelaajaEnsin;
+
+    public Logiikka() {
+        lauta = new Lauta();
+        pelaaja = new Pelaaja("");
+        tietokone = new Pelaaja();
+        noppa1 = new Noppa();
+        noppa2 = new Noppa();
+    }
+
+    public Logiikka(String nimi) {
+        lauta = new Lauta();
+        pelaaja = new Pelaaja(nimi);
+        tietokone = new Pelaaja();
+        noppa1 = new Noppa();
+        noppa2 = new Noppa();
+    }
+
+    public void asetaPelaajaSiirtaaEnsin(boolean pelaajaEnsin) {
+        this.pelaajaEnsin = pelaajaEnsin;
+    }
+
+    public void asetaNappulat() {
+        for (int i = 0; i < 2; i++) {
+            lauta.asetaNappula(new Nappula(pelaaja), 24);
+        }
+        for (int i = 0; i < 5; i++) {
+            lauta.asetaNappula(new Nappula(pelaaja), 13);
+        }
+        for (int i = 0; i < 3; i++) {
+            lauta.asetaNappula(new Nappula(pelaaja), 8);
+        }
+        for (int i = 0; i < 5; i++) {
+            lauta.asetaNappula(new Nappula(pelaaja), 6);
+        }
+        for (int i = 0; i < 2; i++) {
+            lauta.asetaNappula(new Nappula(tietokone), 1);
+        }
+        for (int i = 0; i < 5; i++) {
+            lauta.asetaNappula(new Nappula(tietokone), 12);
+        }
+        for (int i = 0; i < 3; i++) {
+            lauta.asetaNappula(new Nappula(tietokone), 17);
+        }
+        for (int i = 0; i < 5; i++) {
+            lauta.asetaNappula(new Nappula(tietokone), 19);
+        }
+
+    }
+
+    public int ruudunNappulat(int ruutu) {
+        return lauta.nappuloitaRuudussa(ruutu);
+    }
+
+    public boolean ruutuOnTyhja(int ruutu) {
+        return (lauta.nappuloitaRuudussa(ruutu) == 0);
+    }
+
+    public boolean ruutuOnPelaajan(int ruutu) {
+        return lauta.ruutuPelaajalla(ruutu, pelaaja);
+    }
+
+    public boolean ruutuOnTietokoneen(int ruutu) {
+        return lauta.ruutuPelaajalla(ruutu, tietokone);
+    }
+}
---
+++ tavla/src/test/java/tutavla/tavla/logiikka/LogiikkaTest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutavla.tavla.logiikka;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ttuotila@cs
+ */
+public class LogiikkaTest {
+
+    public LogiikkaTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void asetaNappulatLaudalle() {
+        Logiikka logiikka = new Logiikka();
+
+        logiikka.asetaNappulat();
+        assertFalse(logiikka.ruutuOnTyhja(1));
+        assertEquals(logiikka.ruudunNappulat(1), 2);
+        assertTrue(logiikka.ruutuOnTyhja(2));
+        assertTrue(logiikka.ruutuOnTyhja(3));
+        assertTrue(logiikka.ruutuOnTyhja(4));
+        assertTrue(logiikka.ruutuOnTyhja(5));
+        assertFalse(logiikka.ruutuOnTyhja(6));
+        assertEquals(logiikka.ruudunNappulat(6), 5);
+        assertTrue(logiikka.ruutuOnTyhja(7));
+        assertFalse(logiikka.ruutuOnTyhja(8));
+        assertEquals(logiikka.ruudunNappulat(8), 3);
+        assertTrue(logiikka.ruutuOnTyhja(9));
+        assertTrue(logiikka.ruutuOnTyhja(10));
+        assertTrue(logiikka.ruutuOnTyhja(11));
+        assertFalse(logiikka.ruutuOnTyhja(12));
+        assertEquals(logiikka.ruudunNappulat(12), 5);
+        assertFalse(logiikka.ruutuOnTyhja(13));
+        assertEquals(logiikka.ruudunNappulat(13), 5);
+        assertTrue(logiikka.ruutuOnTyhja(14));
+        assertTrue(logiikka.ruutuOnTyhja(15));
+        assertTrue(logiikka.ruutuOnTyhja(16));
+        assertFalse(logiikka.ruutuOnTyhja(17));
+        assertEquals(logiikka.ruudunNappulat(17), 3);
+        assertTrue(logiikka.ruutuOnTyhja(18));
+        assertFalse(logiikka.ruutuOnTyhja(19));
+        assertEquals(logiikka.ruudunNappulat(19), 5);
+        assertTrue(logiikka.ruutuOnTyhja(20));
+        assertTrue(logiikka.ruutuOnTyhja(21));
+        assertTrue(logiikka.ruutuOnTyhja(22));
+        assertTrue(logiikka.ruutuOnTyhja(23));
+        assertFalse(logiikka.ruutuOnTyhja(24));
+        assertEquals(logiikka.ruudunNappulat(24), 2);
+    }
+}
