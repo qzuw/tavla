fe510eef75769ab616c7c01f93b4743aadacdca4
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-08-12 18:58:34 +0300

    vielä vähän refaktorointia

--- tavla/src/main/java/tutavla/tavla/ui/Tekstikayttoliittyma.java
+++ tavla/src/main/java/tutavla/tavla/ui/Tekstikayttoliittyma.java
@@ -17,36 +17,21 @@ import tutavla.tavla.logiikka.Sovelluslogiikka;
 public class Tekstikayttoliittyma implements Kayttoliittyma {
 
     private Sovelluslogiikka svl;
+    private Scanner lukija;
 
     public Tekstikayttoliittyma() {
+        lukija = new Scanner(System.in);
         svl = new Sovelluslogiikka();
     }
 
     @Override
     public void kaynnista() {
-        Scanner lukija = new Scanner(System.in);
 
-        int pelaajamaara = -1;
-
-        while (pelaajamaara < 0 || pelaajamaara > 2) {
-            System.out.println("Montako pelaajaa (0-2)?");
-            try {
-                pelaajamaara = Integer.parseInt(lukija.nextLine());
-            } catch (Exception e) {
-            }
-        }
+        int pelaajamaara = kysyPelaajamaara();
 
         ArrayList<String> pelaajanimet = new ArrayList<>();
 
-        for (int i = 0; i < pelaajamaara; i++) {
-            System.out.println("Pelaajan " + (i + 1) + " nimi?");
-            String nimi = lukija.nextLine();
-            // whitespacen voisi karsia mutta sitten tarvitaan silmukka
-            svl.maaritaPelaaja(i, nimi);
-            // tai 
-//            svl.getSiirtojarjestys().get(i).setNimi(nimi);
-//            svl.getSiirtojarjestys().get(i).setIhminen(true);
-        }
+        kysyPelaajanimet(pelaajamaara);
 
         System.out.println("Paina enter heittääksesi noppaa");
         lukija.nextLine();
@@ -54,34 +39,77 @@ public class Tekstikayttoliittyma implements Kayttoliittyma {
 
         ArrayList<Integer> siirrot;
         ArrayList<Pelaaja> pelaajat;
+
+        arvoAloittaja();
+
+        pelaajat = svl.getSiirtojarjestys();
+
+        valitseVari();
+
+        System.out.println("Peli alkaa");
+        System.out.println("");
+
         while (true) {
-            siirrot = svl.haeSiirrot();
-            pelaajat = svl.getSiirtojarjestys();
-            for (int i = 0; i < 2; i++) {
-                System.out.println(pelaajat.get(i) + "  heitti " + siirrot.get(i));
-            }
-            if (siirrot.size() > 2) {
-                System.out.println("");
-                System.out.println("Molemmat pelaajat heittivät saman luvun, aloittajaa ei voi päättää!");
-                System.out.println("Paina enter heittääksesi noppaa uudestaan");
-                lukija.nextLine();
-                svl.poistaSiirrot();
-                svl.heitaNopat();
-            } else {
-                System.out.println("");
-                if (siirrot.get(0) > siirrot.get(1)) {
-                    System.out.println(pelaajat.get(0) + " heitti suuremman luvun ja aloittaa pelin!");
-                    svl.pelaajaSiirtaaEnsin(true, 0);
-                } else {
-                    System.out.println(pelaajat.get(1) + " heitti suuremman luvun ja aloittaa pelin!");
-                    svl.pelaajaSiirtaaEnsin(true, 1);
-                }
+            pelaaKierros();
+            if (svl.onkoJokuVoittanut()) {
                 break;
             }
         }
+    }
 
+    private void pelaaKierros() {
+        ArrayList<Pelaaja> pelaajat = svl.getSiirtojarjestys();
+
+        System.out.println("Kierros alkaa");
+        System.out.println(svl.pelitilanne());
         System.out.println("");
-        pelaajat = svl.getSiirtojarjestys();
+
+        for (Pelaaja pelaaja : pelaajat) {
+
+            System.out.print("Pelaajan " + pelaaja + " ");
+            if (pelaaja.isMusta()) {
+                System.out.print("(musta)");
+            } else {
+                System.out.print("(valkoinen)");
+            }
+            System.out.println(" vuoro");
+
+            if (pelaaja.isIhminen()) {
+                pelaajaSiirtaa();
+            } else {
+                tietokoneSiirtaa();
+            }
+
+            System.out.println(svl.pelitilanne());
+
+        }
+    }
+
+    private void pelaajaSiirtaa() {
+        ArrayList<Integer> siirrot;
+        while (true) {
+            siirrot = svl.haeSiirrot();
+            if (siirrot.size() < 1) {
+                break;
+            }
+            // yksi siirto
+
+        }
+    }
+
+    private void tietokoneSiirtaa() {
+        ArrayList<Integer> siirrot;
+        while (true) {
+            siirrot = svl.haeSiirrot();
+            if (siirrot.size() < 1) {
+                break;
+            }
+            svl.pelaaTietokone();
+        }
+    }
+
+    private void valitseVari() {
+        ArrayList<Pelaaja> pelaajat = svl.getSiirtojarjestys();
 
         boolean variValittu = false;
 
@@ -141,47 +169,61 @@ public class Tekstikayttoliittyma implements Kayttoliittyma {
         }
 
         System.out.println("");
-        System.out.println("Peli alkaa");
-        System.out.println("");
+    }
 
+    private void arvoAloittaja() {
+        ArrayList<Integer> siirrot;
+        ArrayList<Pelaaja> pelaajat;
         while (true) {
-            System.out.println("Kierros alkaa");
-            System.out.println(svl.pelitilanne());
-            System.out.println("");
-
-            for (Pelaaja pelaaja : pelaajat) {
-
-                System.out.print("Pelaajan " + pelaaja + " ");
-                if (pelaaja.isMusta()) {
-                    System.out.print("(musta)");
+            siirrot = svl.haeSiirrot();
+            pelaajat = svl.getSiirtojarjestys();
+            for (int i = 0; i < 2; i++) {
+                System.out.println(pelaajat.get(i) + "  heitti " + siirrot.get(i));
+            }
+            if (siirrot.size() > 2) {
+                System.out.println("");
+                System.out.println("Molemmat pelaajat heittivät saman luvun, aloittajaa ei voi päättää!");
+                System.out.println("Paina enter heittääksesi noppaa uudestaan");
+                lukija.nextLine();
+                svl.poistaSiirrot();
+                svl.heitaNopat();
+            } else {
+                System.out.println("");
+                if (siirrot.get(0) > siirrot.get(1)) {
+                    System.out.println(pelaajat.get(0) + " heitti suuremman luvun ja aloittaa pelin!");
+                    svl.pelaajaSiirtaaEnsin(true, 0);
                 } else {
-                    System.out.print("(valkoinen)");
+                    System.out.println(pelaajat.get(1) + " heitti suuremman luvun ja aloittaa pelin!");
+                    svl.pelaajaSiirtaaEnsin(true, 1);
                 }
-                System.out.println(" vuoro");
-
-                if (pelaaja.isIhminen()) {
-                    while (true) {
-                        siirrot = svl.haeSiirrot();
-                        if (siirrot.size() < 1) {
-                            break;
-                        }
-                        // yksi siirto
-
-                    }
-                } else {
-                    while (true) {
-                        siirrot = svl.haeSiirrot();
-                        if (siirrot.size() < 1) {
-                            break;
-                        }
-                        svl.pelaaTietokone();
-                    }
-                }
-
-                System.out.println(svl.pelitilanne());
-
+                break;
             }
         }
+        System.out.println("");
+    }
+
+    private void kysyPelaajanimet(int pelaajamaara) {
+        for (int i = 0; i < pelaajamaara; i++) {
+            System.out.println("Pelaajan " + (i + 1) + " nimi?");
+            String nimi = lukija.nextLine();
+            // whitespacen voisi karsia mutta sitten tarvitaan silmukka
+            svl.maaritaPelaaja(i, nimi);
+            // tai
+//            svl.getSiirtojarjestys().get(i).setNimi(nimi);
+//            svl.getSiirtojarjestys().get(i).setIhminen(true);
+        }
+    }
+
+    private int kysyPelaajamaara() {
+        int pelaajamaara = -1;
+        while (pelaajamaara < 0 || pelaajamaara > 2) {
+            System.out.println("Montako pelaajaa (0-2)?");
+            try {
+                pelaajamaara = Integer.parseInt(lukija.nextLine());
+            } catch (Exception e) {
+            }
+        }
+        return pelaajamaara;
     }
 
 }
