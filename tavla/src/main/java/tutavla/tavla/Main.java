a304cfdd96ac8aac7b0ee6c42887bf589ffccea2
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-08-23 17:09:36 +0300

    main

--- tavla/src/main/java/tutavla/tavla/Main.java
+++ tavla/src/main/java/tutavla/tavla/Main.java
@@ -24,7 +24,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Kayttoliittyma kali = new GraafinenKayttoliittyma();
+//        Kayttoliittyma kali = new GraafinenKayttoliittyma();
+        Kayttoliittyma kali = new Tekstikayttoliittyma();
         kali.kaynnista();
     }
 
