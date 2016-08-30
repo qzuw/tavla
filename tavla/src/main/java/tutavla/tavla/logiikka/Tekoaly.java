549382170b11a7890fd3d273e960468a832009ae
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-08-30 17:15:41 +0300

    javadoc

--- tavla/src/main/java/tutavla/tavla/logiikka/Tekoaly.java
+++ tavla/src/main/java/tutavla/tavla/logiikka/Tekoaly.java
@@ -10,13 +10,28 @@ import tutavla.tavla.domain.Pelaaja;
 import tutavla.tavla.domain.Siirto;
 
 /**
+ * Rajapinta Tekoalylle.
  *
  * @author ttuotila
  */
 public interface Tekoaly {
-    
+
+    /**
+     * Valitsee tietokonepelaajalle nappuloiden värin.
+     *
+     * @param siirtojarjestys lista pelaajista
+     * @return satunnaisesti valittu pelaaja
+     */
     public Pelaaja valitseVari(ArrayList<Pelaaja> siirtojarjestys);
-    
+
+    /**
+     * Valitsee yhden siirron ja palauttaa sen.
+     *
+     * @param tietokone siirtava pelaaja
+     * @param plk pelilogiikka
+     * @param siirrot lista noppien arvoja
+     * @return palauttaa tekoälyn valitseman siirron
+     */
     public Siirto pelaa(Pelaaja tietokone, Pelilogiikka plk, ArrayList<Integer> siirrot);
-    
+
 }
