63abaff61d6a4363aa0e69ef46a900325a882788
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-08-31 13:43:46 +0300

    yksi logiikkaluokka lisää

---
+++ tavla/src/main/java/tutavla/tavla/ui/GUILogiikka.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutavla.tavla.ui;
+
+/**
+ * Käyttöliittymän toiminnan logiikkaa hallinnoiva luokka.
+ * 
+ * @author ttuotila
+ */
+public class GUILogiikka {
+    
+}
